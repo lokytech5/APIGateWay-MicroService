51a4bf3926aedf324b005f10ce362118cb41ea27
lokytech5 <lokytech5@gmail.com>
2023-10-11 12:16:58 +0100

    Initial Commit

---
+++ src/main/java/com/lokytech/ApiGateservice/ApiGateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lokytech.ApiGateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokytech/ApiGateservice/ApiGateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokytech.ApiGateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
